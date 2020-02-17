cfedb5a22537fd22f20de1f8a9bb5d1133d43d4f
Aaron Burk <aaron.burk@aa.com>
2020-02-17 08:54:30 -0600

    update datagrid

--- src/components/dataGrid/DataGrid.js
+++ src/components/dataGrid/DataGrid.js
@@ -3,9 +3,21 @@ import './DataGrid.css';
 import { AgGridReact } from "ag-grid-react/lib/agGridReact";
 
 class DataGrid extends Component {
+    state = {
+        columns: [
+            {
+                headerName: 'Category'
+            },
+            {
+                headerName: 'Actual'
+            },
+            3,
+            4
+        ]
+    }
     render() {
         return(
-            <div className="canceled-grid-container">
+            <div className="data-grid-container">
                 <AgGridReact />
             </div>
         )
--- src/components/dataGrid/DataGrid.spec.js
+++ src/components/dataGrid/DataGrid.spec.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import DataGrid from './DataGrid';
 import { AgGridReact } from 'ag-grid-react';
 
-describe('CanceledGrid', () => {
+describe('DataGrid', () => {
     let wrapper;
 
     beforeEach(() => wrapper = shallow(<DataGrid />));
@@ -15,5 +15,17 @@ describe('CanceledGrid', () => {
     it('should contain a matching <AgGridReact /> element', () => {
         expect(wrapper.containsMatchingElement(<AgGridReact />)).toEqual(true);
     });
+
+    it('should contain 4 headings', () => {
+        expect(wrapper.instance().state.columns.length).toEqual(4);
+    });
+
+    it('should contain a header with the name "Category"', () => {
+        expect(wrapper.instance().state.columns[0].headerName).toEqual('Category');
+    });
+
+    it('should contain a header with the name "Actual"', () => {
+        expect(wrapper.instance().state.columns[1].headerName).toEqual('Actual');
+    })
 
 });
